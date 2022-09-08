d9dee4eb604f39e473505b3c27e851561a7fa422
Cyborg <leonardo.delphi@gmail.com>
2022-09-08 01:26:37 -0300

    - added assets  to the project - Player now can move left and rigth....

---
+++ Cyborg_Friends_game/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    bool canmove = true;
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canmove){
+            PlayerMove();
+        }
+    }
+
+    void PlayerMove(){
+
+        float xpos = Input.GetAxis("Horizontal");
+        transform.position += Vector3.right * xpos * speed * Time.deltaTime; 
+
+    }
+}
